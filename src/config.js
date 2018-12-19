8a7d2bbd007d8dbc4a03d4f1cc19e9956a6d9668
ZHAO Xudong <zxdong@gmail.com>
2018-12-19 14:34:06 +0800

    remove get user id

--- src/config.js
+++ src/config.js
@@ -94,17 +94,6 @@ export const phoneNumberSelectors = [
 ]
 
 /**
- * method to get user unique id, could be email, since it is unique
- */
-export function getUserId() {
-  return ''
-  /* example config
-  let arr = document.body.textContent.match(/email: '(.+)'/)
-  return arr ? arr[1] || '' : ''
-  */
-}
-
-/**
  * thirdPartyService config
  * @param {*} serviceName
  */
