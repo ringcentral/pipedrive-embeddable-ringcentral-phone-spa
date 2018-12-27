a3a34dcc01eb5a78d5b1d3ee89cb95f2d7538e51
ZHAO Xudong <zxdong@gmail.com>
2018-12-27 17:14:10 +0800

    Update deps

--- src/config.js
+++ src/config.js
@@ -294,17 +294,3 @@ export async function initThirdParty() {
     notifyRCAuthed()
   }
 }
-
-/**
- * for background.js, check current tab is extension target tab or not
- * @param {object} tab
- */
-export function checkTab(tab) {
-  return /https:\/\/.+\.pipedrive.com\/.+/.test(tab.url)
-  /** url check examples
-    tab.url.startsWith('https://app.hubspot.com') &&
-    !tab.url.startsWith('https://app.hubspot.com/login') &&
-    !tab.url.startsWith('https://app.hubspot.com/myaccounts-beta') &&
-    !tab.url.startsWith('https://app.hubspot.com/developer')
-   */
-}
