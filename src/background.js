61211cae649d0fd35a71bf223bc163daae24880e
ZHAO Xudong <zxdong@gmail.com>
2018-12-27 17:14:10 +0800

    Update deps

--- src/background.js
+++ src/background.js
@@ -1,3 +1,17 @@
 import initBackground from 'ringcentral-embeddable-extension-common/src/spa/background'
-import {checkTab} from './config'
+
+/**
+ * for background.js, check current tab is extension target tab or not
+ * @param {object} tab
+ */
+export function checkTab(tab) {
+  return /https:\/\/.+\.pipedrive.com\/.+/.test(tab.url)
+  /** url check examples
+    tab.url.startsWith('https://app.hubspot.com') &&
+    !tab.url.startsWith('https://app.hubspot.com/login') &&
+    !tab.url.startsWith('https://app.hubspot.com/myaccounts-beta') &&
+    !tab.url.startsWith('https://app.hubspot.com/developer')
+   */
+}
+
 initBackground(checkTab)
